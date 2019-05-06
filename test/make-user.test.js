d9f52a7a30cc9396e2d0f48597171e9054cb10ab
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-06 12:54:12 -0700

    I cloned my repo, created an intitial file structure, and set up my config files

---
+++ test/make-user.test.js
@@ -0,0 +1,12 @@
+const test = QUnit.test;
+
+QUnit.module('make user');
+
+test('create user object from form data', (assert) => {
+    // arrange
+
+    // act
+
+    // assert
+    assert.deepEqual(user, expected);
+});
