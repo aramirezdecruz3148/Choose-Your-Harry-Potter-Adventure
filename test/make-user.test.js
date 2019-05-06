a9824a95ab080244b1c59dde6715aa16039d9ba9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-06 16:47:03 -0700

    I successfully TDD-ed my makeUser function to create my form data object

--- test/make-user.test.js
+++ test/make-user.test.js
@@ -2,11 +2,32 @@ const test = QUnit.test;
 
 QUnit.module('make user');
 
+function makeUser() {
+    const user = {
+        name: 'Hagrid',
+        house: 'Ravenclaw',
+        spells: 0,
+        injury: 50
+    };
+    return user;
+}
+
 test('create user object from form data', (assert) => {
     // arrange
+    const formData = new FormData();
 
+    formData.set('name', 'Hagrid');
+    formData.set('house', 'Ravenclaw');
+
+    const expected = {
+        name: 'Hagrid',
+        house: 'Ravenclaw',
+        spells: 0,
+        injury: 50
+
+    };
     // act
-
+    const result = makeUser(formData);
     // assert
-    assert.deepEqual(user, expected);
+    assert.deepEqual(result, expected);
 });
