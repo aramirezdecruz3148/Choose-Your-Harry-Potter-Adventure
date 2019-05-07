47b92881d47f3c4e05b6c69133cc26ed64a580a1
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-06 17:10:29 -0700

    I successfully TDD-ed my new api utility object which will get the form data, store it using json, and retrieve it

--- test/api.test.js
+++ test/api.test.js
@@ -1,7 +1,20 @@
 const test = QUnit.test;
 
 QUnit.module('api');
+const api = {
+    storage: localStorage,
+    signUp(user) {
+        const json = JSON.stringify(user);
+        api.storage.setItem('user', json);
+    },
+    getUser() {
+        const json = api.storage.getItem('user');
+        if(!json) return null;
+        const user = JSON.parse(json);
+        return user;
+    }
 
+};
 api.storage = sessionStorage;
 QUnit.testStart(() => {
     sessionStorage.clear();
@@ -17,8 +30,9 @@ test('save user and then retrieve the user, roung-trip that shit!', (assert) => 
 
     };
     //Act 
+    api.signUp(expected);
     // Call the function you're testing and set the result to a const
-
+    const result = api.getUser();
     //Assert
     assert.deepEqual(result, expected);
 });
