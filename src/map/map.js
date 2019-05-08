fa879c24ed65785a419eb2a84f6df174704b6b3e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-08 16:31:37 -0700

    I created the function loadProfile encapsulating the code within map.js, then moved it into its own module and imported/exported accordingly

--- src/map/map.js
+++ src/map/map.js
@@ -1,18 +1,4 @@
 import api from '../services/api.js';
+import loadProfile from '../load-profile.js';
 
-//calling on the DOM elements needed
-const userName = document.getElementById('name');
-const houseAvatar = document.getElementById('house');
-const injuryPoints = document.getElementById('injury');
-const spellPoints = document.getElementById('spells');
-
-const user = api.getUser();
-
-if(!user) {
-    window.location = '../../';
-}
-
-userName.textContent = user.name;
-houseAvatar.src = '../../assets/houses/' + user.house + '.jpeg';
-injuryPoints.textContent = user.injury;
-spellPoints.textContent = user.spells;
+loadProfile();
