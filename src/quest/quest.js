c601ab2257ad6aa73713634b808a525475b61b5d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-09 11:44:02 -0700

    I populated my quest.js with necessary imports, functions, DOM references and values, and my for loop to create my choices. I also had to fix a few spelling errors in my quest-data and a name of one of my asset photos

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -1,1 +1,25 @@
 import api from '../services/api.js';
+import createChoice from '../quest/create-choice.js';
+import loadProfile from '../load-profile.js';
+
+loadProfile();
+
+const choiceParent = document.getElementById('choices');
+const questTitle = document.getElementById('quest-title');
+const questImage = document.getElementById('quest-img');
+const questDescription = document.getElementById('description');
+// const questForm = document.getElementById('quest-form');
+
+const searchParams = new URLSearchParams(window.location.search);
+const questId = searchParams.get('id');
+
+let quest = api.getQuest(questId);
+
+questImage.src = '../../assets/quest-photos/' + quest.image;
+questTitle.textContent = quest.title;
+questDescription.textContent = quest.description;
+
+for(let i = 0; i < quest.choices.length; i++) {
+    let choice = createChoice(quest.choices[i]);
+    choiceParent.appendChild(choice);
+}
