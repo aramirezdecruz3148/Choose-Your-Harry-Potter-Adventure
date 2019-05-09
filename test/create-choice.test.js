a60f7b4c90ef2adada0e973c07c18999b5e1811a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-09 09:24:03 -0700

    I moved my create-choice into its own module and export/imported. I also created and began populating my quest.js

--- test/create-choice.test.js
+++ test/create-choice.test.js
@@ -1,21 +1,7 @@
+import createChoice from '../src/quest/create-choice.js';
 const test = QUnit.test;
 
 QUnit.module('createChoice');
-function createChoice(choice) {
-    const label = document.createElement('label');
-    label.textContent = choice.description;
-    
-    
-    const input = document.createElement('input');
-    input.type = 'radio';
-    input.name = 'quest-option';
-    input.value = choice.id;
-    input.setAttribute('required', true);
-
-    label.appendChild(input);
-
-    return label;
-}
 
 test('test to create quest choices in radio button form', (assert) => {
     //Arrange
