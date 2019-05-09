4a1c3dd6cf591d18b1d432c53ce6f8b53bb53bb4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-08 17:12:36 -0700

    I took out my static html in my quest page. I also added a loop in my map.js to loop through my quest links, then append them to the parent element

--- src/map/map.js
+++ src/map/map.js
@@ -1,4 +1,15 @@
 import api from '../services/api.js';
 import loadProfile from '../load-profile.js';
+import createQuestLink from '../map/create-quest-link.js';
+import questArray from '../services/quest-data.js';
 
 loadProfile();
+
+const nav = document.getElementById('quests');
+const quests = api.getQuests(questArray);
+
+for(let i = 0; i < quests.length; i++) {
+    let quest = quests[i];
+    let links = createQuestLink(quest);
+    nav.appendChild(links);
+}
